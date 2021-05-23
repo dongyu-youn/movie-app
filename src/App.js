a5b494c6215f1d691b8deacc16b75152df93ea8d
dongyu-youn <donghe1472@naver.com>
2021-05-23 17:37:12 +0900

    new file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 
-function Food({name, picture}){
+function Food({ name, picture }) {
     return (
-        <div>
-            <h3>I Like {name}</h3>
-        </div>
-    )
-}
-
+      <div>
+        <h2>I like {name}</h2>
+        <img src={picture} />
+      </div>
+    );
+  }
 const foodLike = [
-    {
+    { 
         name: "chicken",
         image: "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTAyMjRfMTgz%2FMDAxNjE0MTc0MTcyNjg0.G97j4rSwS2qxPl1V_4vMufkkx3KElETVnpQNLAAvuLUg.WWp16DTCVimQQy5jIx6t-oLzNdJqMx4eXK8VYpKDeoEg.JPEG.gus0451%2FIMG_2236.JPG&type=a340",
 
-    }
-]
+    },
+];
 
 function App() {
     return  (
